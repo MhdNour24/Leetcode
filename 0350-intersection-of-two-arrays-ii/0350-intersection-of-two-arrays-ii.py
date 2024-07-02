2401d9d0d638d32367cc6a1ab056b0b8dffa6d6c
MHD NOUR EDDIN BASSAL <119541895+MhdNour24@users.noreply.github.com>
2024-07-02 10:12:56 +0300

    Time: 50 ms (48.69%), Space: 16.7 MB (31.35%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,8 +1,21 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        n = []
-        for i in nums1:
-            if i in nums2:
-                n.append(i)
-                nums2.remove(i)
-        return n
+        dic_nums1=defaultdict(int)
+        dic_nums2=defaultdict(int)
+        length_nums1=len(nums1)
+        length_nums2=len(nums2)
+        max_length=max(length_nums1,length_nums2)
+        for i in range(max_length):
+            if i<length_nums1:
+                dic_nums1[nums1[i]]+=1
+            if i<length_nums2:
+                dic_nums2[nums2[i]]+=1
+        new_lst=[]
+        for item,value in dic_nums1.items():
+            value2=dic_nums2[item]
+            pure_value=None
+            if value2>0:
+                pure_value=min(value,value2)
+                new_lst+=[item]*pure_value
+        return new_lst
+
